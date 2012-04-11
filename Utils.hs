a1722276533049a3a3bec5c7544ab1d2ab21cde7
Deian Stefan <deian@cs.stanford.edu>
2012-04-10 18:56:18 -0700

    cleanup Utils
    no main and make file

--- Utils.hs
+++ Utils.hs
@@ -31,48 +31,10 @@ import Data.IterIO.Http (respAddHeader)
 getParamVal :: Monad m => S8.ByteString -> Action t b m String
 getParamVal n = (L8.unpack . paramValue . fromJust) `liftM` param n
 
--- | Get (maybe) paramater value and transform it with @f@
-getMParamVal :: Monad m
-             => (L8.ByteString -> a)
-             -> S8.ByteString
-             -> Action t b m (Maybe a)
-getMParamVal f n = fmap (f . paramValue) `liftM` param n
-
--- | Convert a Param comma separate value to a list of values
-fromCSList :: Param -> [String]
-fromCSList = map (strip . L8.unpack) . L8.split ',' . paramValue
-  where strip = filter (not . isSpace)
-
--- | Get privileges based on current user
-appGetPolicyPriv :: PrivilegeGrantGate dbp => dbp -> Action t b DC DCPrivTCB
-appGetPolicyPriv policy = do
-  app <- principalS `liftM` getHailsApp
-  doGetPolicyPriv policy app
-    where principalS :: String -> Principal
-          principalS = principal
-
--- | Get privilege based on principal
-doGetPolicyPriv :: PrivilegeGrantGate dbp => dbp -> Principal -> Action t b DC DCPrivTCB
-doGetPolicyPriv policy prin = liftLIO $ do
-  gate <- grantPriv policy prin
-  p <- getPrivileges
-  callGate gate p
-
-maybeRead :: Read a => String -> Maybe a
-maybeRead = fmap fst . listToMaybe . reads
-
 with404orJust :: Monad m => Maybe a -> (a -> Action t b m ()) -> Action t b m ()
 with404orJust mval act = case mval of
                            Nothing -> respond404
                            Just val -> act val
-
--- | Convert the body to a labeled key-value Bson document.
-bodyToLDoc :: Action t (DCLabeled L8.ByteString) DC (DCLabeled (Document DCLabel))
-bodyToLDoc = do
- req  <- getHttpReq
- body <- getBody
- liftLIO $ labeledDocI req body
-
 
 --
 -- Flash notifications
